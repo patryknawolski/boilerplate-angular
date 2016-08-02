cdb5d9090549eda403ccd31c1bf3e83d28f259ff
patryk.nawolski <nawolski.patryk@gmail.com>
2016-08-02 23:27:36 +0100

    Initial commit

---
+++ gulp/tasks/build-scripts.js
@@ -0,0 +1,10 @@
+var config = require('../config');
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+// Minifies scripts
+gulp.task('build-scripts', function() {
+	gulp.src(config.scripts.build + '/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest(config.scripts.build));
+});
---
+++ gulp/tasks/eslint.js
@@ -0,0 +1,10 @@
+var config = require('../config');
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('eslint', function() {
+	return gulp.src(config.scripts.src)
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
